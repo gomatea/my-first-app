a312c391edb1124f7e5f9862b7bf7c5cda88f43c
masmo <masmo503@yahoo.co.jp>
2022-01-13 00:40:32 +0900

    フロントエンド側作成

---
+++ src/app/auth/shared/auth.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+// import { products } from "../products";
+
+
+@Injectable()
+export class AuthService{
+
+  // constructor(private http: HttpClient) { }
+
+  // getProducts(): Observable<any>{
+  //   // return products
+  //   return this.http.get('/api/v1/products/')
+  //   // return this.http.get('http://localhost:3001/api/v1/products/')
+  // }
+
+  // getProductById(productId: String): Observable<any>{
+  //   return this.http.get('/api/v1/products/' + productId)
+  //   // return products[productId]
+  // }
+}
